f5f80baf3d21e2d9db7ba554d320700e113e3cd2
Hoang Kha <chauhoangkha442@gmail.com>
2025-06-05 15:03:47 +0700

    gg drive

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { GoogleDriveService } from 'src/auth/google-drive.service';
 
 @Module({
   imports: [ JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService,  GoogleDriveService],
+  providers: [AuthService],
 })
 export class AuthModule {}
