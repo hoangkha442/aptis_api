371f346bc07efb6a3bf30305e3cfa40dfd4d54e1
Hoang Kha <chauhoangkha442@gmail.com>
2025-06-05 15:02:05 +0700

    hard code video

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,16 +18,13 @@ import { BodySignup } from './dto/signup.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { RequestWithUser } from 'src/interfaces';
-import { GoogleDriveService } from 'src/auth/google-drive.service';
-import { Response } from 'express';
-import { google } from 'googleapis';
+import { Request } from 'express';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
-    private readonly driveService: GoogleDriveService,
   ) {}
 
   @HttpCode(200)
@@ -54,56 +51,4 @@ export class AuthController {
   async signup(@Body() bodySignup: BodySignup) {
     return this.authService.signup(bodySignup);
   }
-
-  @Get('google')
-  googleAuth(@Res() res: Response) {
-    const url = this.driveService.getAuthUrl();
-    res.redirect(url);
-  }
-
-  @Get('google/callback')
-  async googleCallback(@Query('code') code: string, @Res() res: Response) {
-    await this.driveService.setCredentials(code);
-    res.redirect('http://localhost:5173/'); // Frontend URL
-  }
-
-  @Get('videos')
-  async listFiles(@Query('folderId') folderId: string) {
-    return await this.driveService.getFilesInFolder(folderId);
-  }
-
-  @Get('video/:fileId')
-  async streamVideo(
-    @Param('fileId') fileId: string,
-    @Res() res: Response,
-    @Req() req: RequestWithUser,
-  ) {
-    const stream = await this.driveService.getDriveFileStream(fileId);
-    res.setHeader('Content-Type', 'video/mp4');
-    stream.pipe(res);
-  }
-
-  // @ApiBearerAuth()
-  // @UseGuards(AuthGuard('jwt'))
-  // @Get('video/stream/:fileId')
-  // async streamVideoMSE(@Param('fileId') fileId: string, @Res() res: Response) {
-  //   const stream = await this.driveService.getDriveFileStream(fileId);
-  //   res.setHeader('Content-Type', 'video/mp4');
-  //   stream.pipe(res);
-  // }
-
-
-  // @Get('video/secure/:fileId')
-  // async streamVideoSecure(
-  //   @Param('fileId') fileId: string,
-  //   // @Req() req: RequestWithUser,
-  //   @Res() res: Response,
-  // ) {
-  //   const stream = await this.driveService.getDriveFileStream(fileId);
-  //   if (!stream) {
-  //     throw new HttpException('File not found', HttpStatus.NOT_FOUND);
-  //   }
-  //   res.setHeader('Content-Type', 'video/mp4');
-  //   stream.pipe(res);
-  // }
 }
